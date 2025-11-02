d2364542d4022c199d6f4022edfb615d94b009e7
Nicolas DENDAUW <ndendauw.nd@gmail.com>
2025-11-02 19:50:19 +0100

    Attr

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,19 +5,40 @@ export const Polaris = {};
  * @type {Object}
  */
 Polaris.abilities = {
-  str: 'Polaris.Ability.Str.long',
-  dex: 'Polaris.Ability.Dex.long',
-  con: 'Polaris.Ability.Con.long',
-  int: 'Polaris.Ability.Int.long',
-  wis: 'Polaris.Ability.Wis.long',
-  cha: 'Polaris.Ability.Cha.long',
+  FOR: 'Polaris.Ability.FOR.long',
+  CON: 'Polaris.Ability.CON.long',
+  COO: 'Polaris.Ability.COO.long',
+  ADA: 'Polaris.Ability.ADA.long',
+  PER: 'Polaris.Ability.PER.long',
+  INT: 'Polaris.Ability.INT.long',
+  VOL: 'Polaris.Ability.VOL.long',
+  PRE: 'Polaris.Ability.PRE.long',
 };
 
 Polaris.abilityAbbreviations = {
-  str: 'Polaris.Ability.Str.abbr',
-  dex: 'Polaris.Ability.Dex.abbr',
-  con: 'Polaris.Ability.Con.abbr',
-  int: 'Polaris.Ability.Int.abbr',
-  wis: 'Polaris.Ability.Wis.abbr',
-  cha: 'Polaris.Ability.Cha.abbr',
+  FOR: 'Polaris.Ability.FOR.abbr',
+  CON: 'Polaris.Ability.CON.abbr',
+  COO: 'Polaris.Ability.COO.abbr',
+  ADA: 'Polaris.Ability.ADA.abbr',
+  PER: 'Polaris.Ability.PER.abbr',
+  INT: 'Polaris.Ability.INT.abbr',
+  VOL: 'Polaris.Ability.VOL.abbr',
+  PRE: 'Polaris.Ability.PRE.abbr'
+};
+Polaris.secondary = {
+  REA: 'Polaris.Ability.REA.long',
+  MDCC: 'Polaris.Ability.MDCC.long',
+  RAC: 'Polaris.Ability.RAC.long',
+  RAD: 'Polaris.Ability.RAD.long',
+  RN: 'Polaris.Ability.RN.long',
+  SOU: 'Polaris.Ability.SOU.long'
+};
+
+Polaris.secondaryAbbreviations = {
+  REA: 'Polaris.Ability.REA.abbr',
+  MDCC: 'Polaris.Ability.MDCC.abbr',
+  RAC: 'Polaris.Ability.RAC.abbr',
+  RAD: 'Polaris.Ability.RAD.abbr',
+  RN: 'Polaris.Ability.RN.abbr',
+  SOU: 'Polaris.Ability.SOU.abbr'
 };
